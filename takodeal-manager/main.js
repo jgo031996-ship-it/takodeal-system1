11502f46c75e441b8e5d230989772ceaa85d5c77
jgo031996-ship-it <jgo031996@gmail.com>
2026-05-19 12:54:25 +0800

    Update main.js

--- takodeal-manager/main.js
+++ takodeal-manager/main.js
@@ -5914,7 +5914,8 @@ window.openPayslipModal = async function(staffName) {
             finalizeBtn.style.background = "#3b82f6"; // Blue
             finalizeBtn.style.cursor = "pointer";
         }
-    }
+    };
+  };
 
     if (!data.rate || data.rate === 0) {
         alert(`⚠️ Warning: ${data.name} does not have a Daily Rate set in their profile!`);
