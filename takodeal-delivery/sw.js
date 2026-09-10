40aa4ae8759399fac458fc55fbe83b1e3a000990
jgo031996-ship-it <jgo031996@gmail.com>
2026-09-10 09:59:23 +0800

    Create sw.js

---
+++ takodeal-delivery/sw.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'rider-app-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './main.js',
+  './logo.jpg'
+];
+
+// Install the service worker and cache the files
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Intercept network requests to keep the app fast
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Return cached version if found, otherwise fetch from internet
+        return response || fetch(event.request);
+      })
+  );
+});
