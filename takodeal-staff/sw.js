90c469ded3a1cba2c3a22fb8710942c31c1fbeee
jgo031996-ship-it <jgo031996@gmail.com>
2026-09-17 13:00:18 +0800

    Add service worker for caching assets

---
+++ takodeal-staff/sw.js
@@ -0,0 +1,9 @@
+const CACHE_NAME = 'takodeal-v1';
+self.addEventListener('install', (e) => {
+    e.waitUntil(caches.open(CACHE_NAME).then((cache) => {
+        return cache.addAll(['/', '/index.html', '/logo.jpg']);
+    }));
+});
+self.addEventListener('fetch', (e) => {
+    e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+});
